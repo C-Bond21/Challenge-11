062cb57f4a8f870218317f2d3931dd288ff376f0
Cayson <111444194+C-Bond21@users.noreply.github.com>
2022-12-19 18:34:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
